72cabd5c52476a501d5b4eb4d66c3dc13f691f02
LauMarcelaRodriguez <86810924+LauMarcelaRodriguez@users.noreply.github.com>
2021-10-23 17:12:47 -0500

    Interfaz principal

---
+++ Sprint2/js/scripts.js
@@ -0,0 +1,5 @@
+let Productos = document.querySelector("#Productos");
+let toggleIcon = document.querySelector("#toggle-icon");
+toggleIcon.addEventListener("click", function(){
+    Productos.classList.toggle('Productos-on')
+})
